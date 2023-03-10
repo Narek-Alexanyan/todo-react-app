abfba9afafc01bbb724b5b9e1f3f23f976484870
Narek Aleksanyan <narek.alexanyan.96@gmail.com>
2023-03-10 14:58:23 +0400

    Updated tailwindcss config, added base styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,26 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      "2xl": { max: "1536px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "567px" },
+      xs: { max: "420px" },
+    },
+    extend: {
+      colors: {
+        "todo-white": "#FFFFFF",
+        "todo-black": "#69665C",
+        "todo-gray": "#B2AFA1",
+        "todo-yellow": "#FFF9DE",
+        "todo-blue": "#D1E5F7",
+        "todo-green": "#DAF2D6",
+        "todo-pink": "#FFCECE",
+        "todo-purple": "#D2CEFF",
+      },
+    },
   },
   plugins: [],
 }
